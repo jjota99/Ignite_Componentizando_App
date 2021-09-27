e3b454d323be8392ee0b20a1f14da13520a49fff
Guilherme Pandolfo <jguilherme1999@gmail.com>
2021-09-27 10:59:48 -0300

    Componentização concluída

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard"
+
+import "../styles/content.scss"
+
+
+interface MovieProps {
+  movie: {
+    imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+  }
+}
+
+
+export function Content(props: MovieProps) {
+  return (
+    <MovieCard key={props.movie.imdbID} title={props.movie.Title} poster={props.movie.Poster} runtime={props.movie.Runtime} rating={props.movie.Ratings[0].Value} />
+    )
 }
